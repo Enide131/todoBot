4e1fe98efc1c35921fb7cc0faf04169b3aba8af9
Enide131 <ruslan150306@gmail.com>
2025-05-28 15:05:47 +0200

    create new database

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ bot.command('add', async (ctx) => {
   const userId = ctx.from.id;
   const input = ctx.message.text.replace('/add ', '').trim();
   const parts = input.split('|');
-  if (parts.length !== 2) return ctx.reply('Неверный формат! Используй:\n/add Текст задачи | ДД.MM ЧЧ:ММ');
+  if (parts.length !== 2) return ctx.reply('Неверный формат! Используй:\n/add Текст задачи | ДД.MM ЧЧ:ММ или просто ЧЧ:ММ)');
 
 
 
@@ -58,21 +58,18 @@ bot.command('add', async (ctx) => {
   const now = dayjs();
   let time = dayjs(`${dateTimeStr} ${now.year()}`, 'DD.MM HH:mm YYYY');
 
-  if (dateTimeStr.includes(' ')) {
-  time = dayjs(`${dateTimeStr} ${now.year()}`, 'DD.MM HH:mm YYYY');
-  if (time.isValid() && time.isBefore(now)) {
-    time = time.add(1, 'year');
+  if (dateTimeStr.includes(' ')) 
+  {
+    time = dayjs(`${dateTimeStr} ${now.year()}`, 'DD.MM HH:mm YYYY');
+    if (time.isValid() && time.isBefore(now)) time = time.add(1, 'year');
+  } 
+  else 
+  {
+    time = dayjs(`${now.format('DD.MM.YYYY')} ${dateTimeStr}`, 'DD.MM.YYYY HH:mm');
+    if (time.isValid() && time.isBefore(now)) time = time.add(1, 'day');
   }
-} else {
-  time = dayjs(`${now.format('DD.MM.YYYY')} ${dateTimeStr}`, 'DD.MM.YYYY HH:mm');
-  if (time.isValid() && time.isBefore(now)) {
-    time = time.add(1, 'day');
-  }
-}
 
-  if (!time.isValid()) {
-    return ctx.reply('Неверная дата/время. Используй формат ДД.MM ЧЧ:ММ');
-  }
+  if (!time.isValid()) return ctx.reply('Неверная дата/время. Используй формат ДД.MM ЧЧ:ММ');
 
   const result = await db.query(
     'INSERT INTO tasks(user_id, text, time) VALUES ($1, $2, $3) RETURNING id',
